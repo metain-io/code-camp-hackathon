2e4e1b5357d9618c6ef61106d0f8d6788bd4ac53
Quang Doan <quang.doan@metain.io>
2022-11-25 18:13:25 +0700

    - WIP

--- client-app/src/pages/_app.tsx
+++ client-app/src/pages/_app.tsx
@@ -3,11 +3,15 @@ import type { AppProps } from 'next/app';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@app/styles/default.scss';
 import '@app/styles/reset.scss';
+import { SolanaWalletContextProvider } from '@auth/components';
+import '@solana/wallet-adapter-react-ui/styles.css';
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <MainLayout>
-            <Component {...pageProps} />
-        </MainLayout>
+        <SolanaWalletContextProvider>
+            <MainLayout>
+                <Component {...pageProps} />
+            </MainLayout>
+        </SolanaWalletContextProvider>
     );
 }

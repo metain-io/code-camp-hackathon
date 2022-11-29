97a15f135552da730c681143ed13b1a787073cf6
Quang Doan <quang.doan@metain.io>
2022-11-30 00:46:53 +0700

    Update index.tsx

--- client-app/src/modules/app/layouts/auth-layout/index.tsx
+++ client-app/src/modules/app/layouts/auth-layout/index.tsx
@@ -13,21 +13,7 @@ const AuthLayout = (props: AuthLayoutProps) => {
                 <div className="row">
                     <div id={styles.left_wrapper} className="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">
                         <div className="row">
-                            <div className="col-xl-10 col-lg-10 col-md-12 col-sm-12 col-12">
-                                {/* {deviceType !== 'browser' && (
-                                    <div className={styles.div_1}>
-                                        <Image className={styles.image_101} src="/image/zero/image-logo-fill.png" />
-                                        <SelectBox
-                                            className={styles.selectbox_1}
-                                            value={[i18n.language === 'en' ? languageList[0] : languageList[1]]}
-                                            options={languageList}
-                                            dropdownWidth="16rem"
-                                            C_onChange={(value) => changeLanguage(value[0].value)}
-                                        />
-                                    </div>
-                                )} */}
-                                {children}
-                            </div>
+                            <div className="col-xl-10 col-lg-10 col-md-12 col-sm-12 col-12">{children}</div>
                         </div>
                     </div>
                     <div id={styles.right_wrapper} className="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12">

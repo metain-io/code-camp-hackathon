36196ecfbf104efdb7c8cc7dd0d1078981cee52b
Chung Nguyen <nguyentruongchung@gmail.com>
2022-11-30 18:19:35 +0700

    wip on offering smc

--- tools/offering/programs/offering/src/lib.rs
+++ tools/offering/programs/offering/src/lib.rs
@@ -1,15 +1,103 @@
 use anchor_lang::prelude::*;
+use anchor_spl::{associated_token::AssociatedToken, token::{CloseAccount, Mint, Token, TokenAccount, Transfer}};
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("AbHafG3NvBumgzKZnKFZnvTcmM2zVFQfBmn4kHbWYn3d");
 
 #[program]
 pub mod offering {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn set_treasury(ctx: Context<SetTreasury>, application_idx: u64) -> Result<()> {
+        let appState = &mut ctx.accounts.application_state;
+        appState.treasury = ctx.accounts.treasury.key().clone();
         Ok(())
     }
+
+    // pub fn buy(ctx: Context<Buy>, currency: String, amount: u32) -> Result<()> {
+    //     msg!("Buy {} NFTs with {}", amount, currency);
+
+    //     // // Transfer amount of currency to treasurer
+    //     // let transfer_instruction = Transfer{
+    //     //     from: escrow_wallet.to_account_info(),
+    //     //     to: destination_wallet,
+    //     //     authority: state.to_account_info(),
+    //     // };
+    //     // let cpi_ctx = CpiContext::new_with_signer(
+    //     //     token_program.to_account_info(),
+    //     //     transfer_instruction,
+    //     //     outer.as_slice(),
+    //     // );
+    //     // anchor_spl::token::transfer(cpi_ctx, amount)?;
+
+    //     Ok(())
+    // }
+}
+
+#[account]
+#[derive(Default)]
+pub struct State {
+    pub treasury: Pubkey
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(application_idx: u64)]
+pub struct SetTreasury<'info> {
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    /// CHECK: Treasury account is setup one-time only
+    pub treasury: AccountInfo<'info>,
+
+    // Derived PDAs
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + 32,
+        seeds = [b"state".as_ref(), signer.key().as_ref(), application_idx.to_le_bytes().as_ref()],
+        bump
+    )]
+    application_state: Account<'info, State>,
+
+    pub system_program: Program<'info, System>
+}
+
+// #[derive(Accounts)]
+// #[instruction(application_idx: u64, state_bump: u8, wallet_bump: u8)]
+// pub struct InitializeNewGrant<'info> {
+
+//     // Derived PDAs
+//     #[account(
+//         init,
+//         payer = user_sending,
+//         seeds=[b"state".as_ref(), user_sending.key().as_ref(), user_receiving.key.as_ref(), mint_of_token_being_sent.key().as_ref(), application_idx.to_le_bytes().as_ref()],
+//         bump = state_bump,
+//     )]
+//     application_state: Account<'info, State>,
+//     #[account(
+//         init,
+//         payer = user_sending,
+//         seeds=[b"wallet".as_ref(), user_sending.key().as_ref(), user_receiving.key.as_ref(), mint_of_token_being_sent.key().as_ref(), application_idx.to_le_bytes().as_ref()],
+//         bump = wallet_bump,
+//         token::mint=mint_of_token_being_sent,
+//         token::authority=application_state,
+//     )]
+//     escrow_wallet_state: Account<'info, TokenAccount>,
+
+//     // Users and accounts in the system
+//     #[account(mut)]
+//     user_sending: Signer<'info>,                     // Alice
+//     user_receiving: AccountInfo<'info>,              // Bob
+//     mint_of_token_being_sent: Account<'info, Mint>,  // USDC
+
+//     // Alice's USDC wallet that has already approved the escrow wallet
+//     #[account(
+//         mut,
+//         constraint=wallet_to_withdraw_from.owner == user_sending.key(),
+//         constraint=wallet_to_withdraw_from.mint == mint_of_token_being_sent.key()
+//     )]
+//     wallet_to_withdraw_from: Account<'info, TokenAccount>,
+
+//     // Application level accounts
+//     system_program: Program<'info, System>,
+//     token_program: Program<'info, Token>,
+//     rent: Sysvar<'info, Rent>,
+// }
--- tools/offering/tests/offering.ts
+++ tools/offering/tests/offering.ts
@@ -1,16 +1,69 @@
+import { expect } from "chai";
+
+import * as web3 from "@solana/web3.js";
+import * as token from "@solana/spl-token";
+
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { Offering } from "../target/types/offering";
 
+const TREASURY_WALLET = "621i9tL4tRBgt2PRbHynqSdYxPEd3KvpVkKX3chge3mU";
+
+export async function createNewMint(connection: web3.Connection, payer: web3.Keypair, mintAuthority: web3.PublicKey, freezeAuthority: web3.PublicKey, decimals: number): Promise<web3.PublicKey> {
+  const tokenMint = await token.createMint(connection, payer, mintAuthority, freezeAuthority, decimals);
+
+  console.log(`Token Mint: https://explorer.solana.com/address/${tokenMint}?cluster=devnet`);
+
+  return tokenMint;
+}
+
 describe("offering", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  const connection = provider.connection;
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.Offering as Program<Offering>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  const payer: web3.Keypair = (provider.wallet as any).payer;
+  const admin = anchor.web3.Keypair.generate();
+  const buyer = anchor.web3.Keypair.generate();
+
+  let mintUSDT;
+
+  const getPdaParams = async (connection: anchor.web3.Connection, signer: anchor.web3.PublicKey): Promise<any> => {
+    const uid = new anchor.BN(parseInt((Date.now() / 1000).toString()));
+    const uidBuffer = uid.toBuffer("le", 8);
+
+    let [statePubKey, stateBump] = await anchor.web3.PublicKey.findProgramAddress([Buffer.from("state"), signer.toBuffer(), uidBuffer], program.programId);
+    return {
+      stateBump,
+
+      idx: uid,
+      stateKey: statePubKey
+    };
+  };
+
+  it("Initialize", async () => {
+    mintUSDT = await createNewMint(connection, payer, admin.publicKey, admin.publicKey, 6);
+    expect(mintUSDT.toBase58());
+  });
+
+  it("Setup", async () => {
+    const pda = await getPdaParams(connection, provider.wallet.publicKey);
+
+    const tx = await program.methods.setTreasury(new anchor.BN(pda.idx)).accounts({
+      signer: provider.wallet.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+      applicationState: pda.stateKey,
+      treasury: new anchor.web3.PublicKey(TREASURY_WALLET)
+    }).rpc();
+
+    const applicationState = await program.account.state.fetch(pda.stateKey);
+    console.log(applicationState.treasury.toBase58())
+  });
+
+  it("Buying", async () => {
+    // const tx = await program.methods.setTreasury(101).accounts({ user: provider.wallet.publicKey }).rpc();
   });
 });

6614b744458f0f349ccc7ae454e6193401f0013b
Quang Doan <quang.doan@metain.io>
2022-12-01 18:09:52 +0700

    - WIP

---
+++ client-app/src/modules/opportunity-trust-portfolio/components/opportunity-trust-portfoio-detail-provider/index.tsx
@@ -0,0 +1,21 @@
+import React, { PropsWithChildren } from 'react';
+import { OpportunityTrustPortfolioDetailContext } from './opportunity-trust-portfolio-detail-context';
+import { useOpportunityTrustPortfolioDetail } from './use-opportunity-trust-portfolio-detail';
+
+type OpportunityTrustPortfolioDetailProps = PropsWithChildren<{
+    id?: string;
+}>;
+
+const OpportunityTrustPortfolioDetailProvider = (props: OpportunityTrustPortfolioDetailProps) => {
+    const { id = 'VOT1', children } = props;
+
+    const { data } = useOpportunityTrustPortfolioDetail(id);
+
+    return (
+        <OpportunityTrustPortfolioDetailContext.Provider value={data}>
+            {children}
+        </OpportunityTrustPortfolioDetailContext.Provider>
+    );
+};
+
+export { OpportunityTrustPortfolioDetailProvider };
---
+++ client-app/src/modules/opportunity-trust-portfolio/components/opportunity-trust-portfoio-detail-provider/opportunity-trust-portfolio-detail-context.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+type OpportunityTrustPortfolioDetailContextState = {
+    id: string;
+    name: string;
+    type: string;
+    address: string;
+    price: number;
+    targetRaise: number;
+    valuationDate: number;
+    expectedNetRentalYield: number;
+    expectedPropertyValueAppreciation: number;
+    holdingTimes: string;
+    operateStatus: string;
+    imageUrls: Array<string>;
+    partnerResponsibilitiess: Array<{
+        name: string;
+        partners: Array<{
+            name: string;
+            imageUrl: string;
+        }>;
+    }>;
+    documents: Array<{
+        name: string;
+        href: string;
+    }>;
+    description: string;
+    highlightProperties: Array<{
+        name: string;
+        content: string;
+    }>;
+    roadmap: Array<{
+        title: string;
+        startDate: string;
+        endDate: string;
+        timeLabel: string;
+    }>;
+    mapMarker: {
+        lat: number;
+        long: number;
+        iconUrl: string;
+        title: string;
+    };
+};
+
+const OpportunityTrustPortfolioDetailContext = React.createContext({} as OpportunityTrustPortfolioDetailContextState);
+
+export { OpportunityTrustPortfolioDetailContext };
+export const useOpportunityTrustPortfolioDetailContext = () => React.useContext(OpportunityTrustPortfolioDetailContext);

a8287618e2151e74986ee7b231c1de362adec974
VTTruong287 <truong.vt287@gmail.com>
2022-12-02 15:12:58 +0700

    [Metain][SolCamp] - Optimize with Crypto Wallet Interface

--- client-app/src/pages/dashboard/hooks/use-dashboard.tsx
+++ client-app/src/pages/dashboard/hooks/use-dashboard.tsx
@@ -5,77 +5,46 @@ import { SelectBox_Component } from '../components/select-box';
 import { useSelector } from 'react-redux';
 import { selectLoginWalletAddress } from '@auth/redux/login/slice';
 import { Table_Component } from '../components/table/type';
+import WalletService from '@crypto-wallet/services/crypto-wallet-service';
+import logger from '@libs/logger';
 
 const useDashboard = () => {
-    const DECIMAL_NUMBER = BigInt(Math.pow(10, 6));
-    const TOKEN_CONFIG: Array<SelectBox_Component.Value> = [
-        {
-            label: 'USDT',
-            value: 'A7yGbWrtgTjXVdxky86CSEyfH6Jy388RYFWfZsH2D8hr',
-            icon: '/svg/icon-token-usdt.svg',
-        },
-        {
-            label: 'USDC',
-            value: '3GUqiPovczNg1KoZg5FovwRZ4KPFb95UGZCCTPFb9snc',
-            icon: '/svg/icon-token-usdc.svg',
-        },
-    ];
+    const TOKEN_CONFIG: Array<SelectBox_Component.Value> = WalletService._currentWallet?.tokenForSelect || [];
     const walletAddress = useSelector(selectLoginWalletAddress);
 
     const [balances, setBalances] = React.useState<{ [name: string]: number | bigint }>({
         [TOKEN_CONFIG[0].label]: 0,
         [TOKEN_CONFIG[1].label]: 0,
     });
-    const [userTokenList, setUserTokenList] = React.useState<Array<Table_Component.Row>>([])
+    const [userTokenList, setUserTokenList] = React.useState<Array<Table_Component.Row>>([]);
 
-    const getBalances = async (tokenAddress: string) => {
-        const tmpBalances: { [name: string]: number | bigint } = {};
-        try {
-            const connection = new web3.Connection(web3.clusterApiUrl('devnet'), 'confirmed');
-            const tokenAccounts = await connection.getTokenAccountsByOwner(new web3.PublicKey(walletAddress), {
-                programId: SPL.TOKEN_PROGRAM_ID,
-            });
-            const solAmount = await connection.getBalance(new web3.PublicKey(walletAddress));
-
-            tmpBalances['SOL'] = BigInt(solAmount) / BigInt(web3.LAMPORTS_PER_SOL);
-            tokenAccounts?.value?.forEach((tokenAccount) => {
-                const accountData = SPL.AccountLayout.decode(tokenAccount.account.data);
-
-                const relatedToken = TOKEN_CONFIG.find((item, index) => {
-                    return item.value == accountData.mint.toBase58();
-                });
-                if (relatedToken) {
-                    tmpBalances[relatedToken.label] = accountData.amount / DECIMAL_NUMBER;
-                }
-            });
-            console.log('================== getBalances - BALANCE: ', tmpBalances);
-
-            setBalances(tmpBalances);
-        } catch (error: any) {
-            console.log('============ getBalances - ERROR: ', error);
-            return {};
-        }
-        return tmpBalances;
+    const getBalances = async () => {
+        return (
+            (await WalletService._currentWallet?.getBalances(
+                walletAddress
+            )) || {}
+        );
     };
 
     const getAndConvertBalance2TokenTableData = async () => {
-        const tmpBalances = await getBalances(walletAddress);
+        const tmpBalances = await getBalances();
         const entries = Object.entries(tmpBalances);
         const tokenTableData: Array<Table_Component.Row> = [];
 
         entries &&
-        entries.length > 0 &&
-        entries.forEach((item, idx) => {
-            tokenTableData.push({
-                name: item[0],
-                symbol: item[0],
-                current_price: item[0] == 'SOL' ? 16 : 1,
-                amount: item[1]?.toString() || '0',
+            entries.length > 0 &&
+            entries.forEach((item, idx) => {
+                tokenTableData.push({
+                    name: item[0],
+                    symbol: item[0],
+                    current_price: item[0] == 'SOL' ? 16 : 1,
+                    amount: item[1]?.toString() || '0',
+                });
             });
-        });
+        setBalances(tmpBalances);
         setUserTokenList(tokenTableData);
 
-        console.log('============ getAndConvertBalance2TokenTableData - RS: ', tokenTableData);
+        logger.debug('============ getAndConvertBalance2TokenTableData - RS: ', tokenTableData);
 
         return tokenTableData;
     };

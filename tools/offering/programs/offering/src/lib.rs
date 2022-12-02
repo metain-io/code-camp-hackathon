1e689e94ba5bcad4300577185ef4dadf302e5bc1
Chung Nguyen <nguyentruongchung@gmail.com>
2022-12-02 18:33:08 +0700

    update smart contract: ability to swap usd with nft

--- tools/offering/programs/offering/src/lib.rs
+++ tools/offering/programs/offering/src/lib.rs
@@ -3,101 +3,213 @@ use anchor_spl::{associated_token::AssociatedToken, token::{CloseAccount, Mint, 
 
 declare_id!("AbHafG3NvBumgzKZnKFZnvTcmM2zVFQfBmn4kHbWYn3d");
 
+fn transfer_escrow_out<'info>(
+    user_sending: AccountInfo<'info>,
+    user_receiving: AccountInfo<'info>,
+    mint_of_token_being_sent: AccountInfo<'info>,
+    escrow_wallet: &mut Account<'info, TokenAccount>,
+    application_idx: u64,
+    state: AccountInfo<'info>,
+    state_bump: u8,
+    token_program: AccountInfo<'info>,
+    destination_wallet: AccountInfo<'info>,
+    amount: u64
+) -> Result<()> {
+
+    // Nothing interesting here! just boilerplate to compute our signer seeds for
+    // signing on behalf of our PDA.
+    let bump_vector = state_bump.to_le_bytes();
+    let mint_of_token_being_sent_pk = mint_of_token_being_sent.key().clone();
+    let application_idx_bytes = application_idx.to_le_bytes();
+    let inner = vec![
+        b"state".as_ref(),
+        user_sending.key.as_ref(),
+        mint_of_token_being_sent_pk.as_ref(),
+        application_idx_bytes.as_ref(),
+        bump_vector.as_ref(),
+    ];
+    let outer = vec![inner.as_slice()];
+
+    // Perform the actual transfer
+    let transfer_instruction = Transfer{
+        from: escrow_wallet.to_account_info(),
+        to: destination_wallet,
+        authority: state.to_account_info(),
+    };
+    let cpi_ctx = CpiContext::new_with_signer(
+        token_program.to_account_info(),
+        transfer_instruction,
+        outer.as_slice(),
+    );
+    anchor_spl::token::transfer(cpi_ctx, amount)?;
+
+    Ok(())
+}
+
 #[program]
 pub mod offering {
     use super::*;
 
-    pub fn set_treasury(ctx: Context<SetTreasury>, application_idx: u64) -> Result<()> {
-        let appState = &mut ctx.accounts.application_state;
-        appState.treasury = ctx.accounts.treasury.key().clone();
+    pub fn deposit_nft(ctx: Context<DepositNft>, application_idx: u64, state_bump: u8, amount: u64) -> Result<()> {
+        // Set the state attributes
+        let state = &mut ctx.accounts.application_state;
+        state.idx = application_idx;
+
+        let bump_vector = state_bump.to_le_bytes();
+        let mint_of_token_being_sent_pk = ctx.accounts.mint_of_token_being_sent.key().clone();
+        let application_idx_bytes = application_idx.to_le_bytes();
+        let inner = vec![
+            b"state".as_ref(),
+            ctx.accounts.treasurer.key.as_ref(),
+            mint_of_token_being_sent_pk.as_ref(),
+            application_idx_bytes.as_ref(),
+            bump_vector.as_ref(),
+        ];
+        let outer = vec![inner.as_slice()];
+
+        let transfer_instruction = Transfer{
+            from: ctx.accounts.wallet_to_withdraw_from.to_account_info(),
+            to: ctx.accounts.escrow_wallet_state.to_account_info(),
+            authority: ctx.accounts.treasurer.to_account_info(),
+        };
+        let cpi_ctx = CpiContext::new_with_signer(
+            ctx.accounts.token_program.to_account_info(),
+            transfer_instruction,
+            outer.as_slice()
+        );
+
+        anchor_spl::token::transfer(cpi_ctx, amount)?;
+
         Ok(())
     }
 
-    // pub fn buy(ctx: Context<Buy>, currency: String, amount: u32) -> Result<()> {
-    //     msg!("Buy {} NFTs with {}", amount, currency);
+    pub fn buy(ctx: Context<Buy>, application_idx: u64, state_bump: u8, wallet_bump: u8, amount: u64) -> Result<()> {
+        let transfer_instruction = Transfer{
+            from: ctx.accounts.wallet_to_withdraw_from.to_account_info(),
+            to: ctx.accounts.wallet_to_deposit_to.to_account_info(),
+            authority: ctx.accounts.buyer.to_account_info(),
+        };
 
-    //     // // Transfer amount of currency to treasurer
-    //     // let transfer_instruction = Transfer{
-    //     //     from: escrow_wallet.to_account_info(),
-    //     //     to: destination_wallet,
-    //     //     authority: state.to_account_info(),
-    //     // };
-    //     // let cpi_ctx = CpiContext::new_with_signer(
-    //     //     token_program.to_account_info(),
-    //     //     transfer_instruction,
-    //     //     outer.as_slice(),
-    //     // );
-    //     // anchor_spl::token::transfer(cpi_ctx, amount)?;
+        let cpi_ctx = CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            transfer_instruction
+        );
 
-    //     Ok(())
-    // }
+        anchor_spl::token::transfer(cpi_ctx, amount * 10000000)?;
+
+        transfer_escrow_out(
+            ctx.accounts.treasurer.to_account_info(),
+            ctx.accounts.buyer.to_account_info(),
+            ctx.accounts.mint_of_token_being_sent.to_account_info(),
+            &mut ctx.accounts.escrow_wallet_state,
+            application_idx,
+            ctx.accounts.application_state.to_account_info(),
+            state_bump,
+            ctx.accounts.token_program.to_account_info(),
+            ctx.accounts.wallet_to_send_to.to_account_info(),
+            amount
+        )?;
+
+        Ok(())
+    }
 }
 
 #[account]
 #[derive(Default)]
 pub struct State {
-    pub treasury: Pubkey
+    // A primary key that allows us to derive other important accounts
+    idx: u64
 }
 
 #[derive(Accounts)]
-#[instruction(application_idx: u64)]
-pub struct SetTreasury<'info> {
-    #[account(mut)]
-    pub signer: Signer<'info>,
-    /// CHECK: Treasury account is setup one-time only
-    pub treasury: AccountInfo<'info>,
-
+#[instruction(application_idx: u64, state_bump: u8, amount: u64)]
+pub struct DepositNft<'info> {
     // Derived PDAs
     #[account(
         init,
-        payer = signer,
-        space = 8 + 32,
-        seeds = [b"state".as_ref(), signer.key().as_ref(), application_idx.to_le_bytes().as_ref()],
-        bump
+        space = 8 + 8,
+        payer = treasurer,
+        seeds=[b"state".as_ref(), treasurer.key().as_ref(), mint_of_token_being_sent.key().as_ref(), application_idx.to_le_bytes().as_ref()],
+        bump,
     )]
     application_state: Account<'info, State>,
 
-    pub system_program: Program<'info, System>
+    #[account(
+        init,
+        payer = treasurer,
+        seeds=[b"wallet".as_ref(), treasurer.key().as_ref(), mint_of_token_being_sent.key().as_ref(), application_idx.to_le_bytes().as_ref()],
+        bump,
+        token::mint=mint_of_token_being_sent,
+        token::authority=application_state,
+    )]
+    escrow_wallet_state: Account<'info, TokenAccount>,
+
+    // Users and accounts in the system
+    #[account(mut)]
+    treasurer: Signer<'info>,                     // VOT manager
+    mint_of_token_being_sent: Account<'info, Mint>,  // NFT
+    mint_of_token_being_paid: Account<'info, Mint>, // USDC
+
+    // VOT wallet that has already approved the escrow wallet
+    #[account(
+        mut,
+        constraint=wallet_to_withdraw_from.owner == treasurer.key(),
+        constraint=wallet_to_withdraw_from.mint == mint_of_token_being_sent.key()
+    )]
+    wallet_to_withdraw_from: Account<'info, TokenAccount>,
+
+    // Application level accounts
+    system_program: Program<'info, System>,
+    token_program: Program<'info, Token>,
+    rent: Sysvar<'info, Rent>,
 }
 
-// #[derive(Accounts)]
-// #[instruction(application_idx: u64, state_bump: u8, wallet_bump: u8)]
-// pub struct InitializeNewGrant<'info> {
+#[derive(Accounts)]
+#[instruction(application_idx: u64, state_bump: u8, wallet_bump: u8, amount: u64)]
+pub struct Buy<'info> {
+    // Derived PDAs
+    #[account(
+        mut,
+        seeds=[b"state".as_ref(), treasurer.key().as_ref(), mint_of_token_being_sent.key().as_ref(), application_idx.to_le_bytes().as_ref()],
+        bump = state_bump
+    )]
+    application_state: Account<'info, State>,
 
-//     // Derived PDAs
-//     #[account(
-//         init,
-//         payer = user_sending,
-//         seeds=[b"state".as_ref(), user_sending.key().as_ref(), user_receiving.key.as_ref(), mint_of_token_being_sent.key().as_ref(), application_idx.to_le_bytes().as_ref()],
-//         bump = state_bump,
-//     )]
-//     application_state: Account<'info, State>,
-//     #[account(
-//         init,
-//         payer = user_sending,
-//         seeds=[b"wallet".as_ref(), user_sending.key().as_ref(), user_receiving.key.as_ref(), mint_of_token_being_sent.key().as_ref(), application_idx.to_le_bytes().as_ref()],
-//         bump = wallet_bump,
-//         token::mint=mint_of_token_being_sent,
-//         token::authority=application_state,
-//     )]
-//     escrow_wallet_state: Account<'info, TokenAccount>,
+    #[account(
+        mut,
+        seeds=[b"wallet".as_ref(), treasurer.key().as_ref(), mint_of_token_being_sent.key().as_ref(), application_idx.to_le_bytes().as_ref()],
+        bump = wallet_bump,
+        token::mint=mint_of_token_being_sent,
+        token::authority=application_state,
+    )]
+    escrow_wallet_state: Account<'info, TokenAccount>,
 
-//     // Users and accounts in the system
-//     #[account(mut)]
-//     user_sending: Signer<'info>,                     // Alice
-//     user_receiving: AccountInfo<'info>,              // Bob
-//     mint_of_token_being_sent: Account<'info, Mint>,  // USDC
+    #[account(
+        mut,
+        constraint=wallet_to_withdraw_from.owner == buyer.key()
+    )]
+    wallet_to_withdraw_from: Account<'info, TokenAccount>,
 
-//     // Alice's USDC wallet that has already approved the escrow wallet
-//     #[account(
-//         mut,
-//         constraint=wallet_to_withdraw_from.owner == user_sending.key(),
-//         constraint=wallet_to_withdraw_from.mint == mint_of_token_being_sent.key()
-//     )]
-//     wallet_to_withdraw_from: Account<'info, TokenAccount>,
+    #[account(
+        mut,
+        constraint=wallet_to_deposit_to.owner == treasurer.key()
+    )]
+    wallet_to_deposit_to: Account<'info, TokenAccount>,
 
-//     // Application level accounts
-//     system_program: Program<'info, System>,
-//     token_program: Program<'info, Token>,
-//     rent: Sysvar<'info, Rent>,
-// }
+    #[account(
+        mut,
+        associated_token::mint = mint_of_token_being_sent.key(),
+        associated_token::authority = buyer.key(),
+    )]
+    wallet_to_send_to: Account<'info, TokenAccount>,
+
+    #[account(mut)]
+    buyer: Signer<'info>,
+    mint_of_token_being_sent: Account<'info, Mint>,
+
+    /// CHECK: It is always safe to be beneficiary
+    treasurer: AccountInfo<'info>,
+
+    system_program: Program<'info, System>,
+    token_program: Program<'info, Token>
+}
--- tools/offering/tests/offering.ts
+++ tools/offering/tests/offering.ts
@@ -4,10 +4,12 @@ import * as web3 from "@solana/web3.js";
 import * as token from "@solana/spl-token";
 
 import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
+import { Program, Spl } from "@project-serum/anchor";
 import { Offering } from "../target/types/offering";
 
-const TREASURY_WALLET = "621i9tL4tRBgt2PRbHynqSdYxPEd3KvpVkKX3chge3mU";
+const NFT_TOTAL_SUPPLY = 200000;
+
+let pda;
 
 export async function createNewMint(connection: web3.Connection, payer: web3.Keypair, mintAuthority: web3.PublicKey, freezeAuthority: web3.PublicKey, decimals: number): Promise<web3.PublicKey> {
   const tokenMint = await token.createMint(connection, payer, mintAuthority, freezeAuthority, decimals);
@@ -15,6 +17,32 @@ export async function createNewMint(connection: web3.Connection, payer: web3.Key
   console.log(`Token Mint: https://explorer.solana.com/address/${tokenMint}?cluster=devnet`);
 
   return tokenMint;
+}
+
+export async function mintTokens(connection: web3.Connection, payer: web3.Keypair, mint: web3.PublicKey, destination: web3.PublicKey, authority: web3.Keypair, amount: number | bigint) {
+  const transactionSignature = await token.mintTo(connection, payer, mint, destination, authority, amount);
+
+  console.log(`Mint Token Transaction: https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`);
+}
+
+export async function createTokenAccount(connection: web3.Connection, payer: web3.Keypair, mint: web3.PublicKey, owner: web3.PublicKey) {
+  const tokenAccount = await token.getOrCreateAssociatedTokenAccount(connection, payer, mint, owner);
+
+  console.log(`Token Account: https://explorer.solana.com/address/${tokenAccount.address}?cluster=devnet`);
+
+  return tokenAccount;
+}
+
+export async function fundSolana(provider: anchor.AnchorProvider, account: web3.PublicKey, amount: number) {
+  let txFund = new anchor.web3.Transaction();
+  txFund.add(
+    anchor.web3.SystemProgram.transfer({
+      fromPubkey: provider.wallet.publicKey,
+      toPubkey: account,
+      lamports: amount * anchor.web3.LAMPORTS_PER_SOL
+    })
+  );
+  await provider.sendAndConfirm(txFund);
 }
 
 describe("offering", () => {
@@ -27,43 +55,107 @@ describe("offering", () => {
 
   const payer: web3.Keypair = (provider.wallet as any).payer;
   const admin = anchor.web3.Keypair.generate();
+  const treasurer = anchor.web3.Keypair.generate();
   const buyer = anchor.web3.Keypair.generate();
 
   let mintUSDT;
+  let mintVOT1;
 
-  const getPdaParams = async (connection: anchor.web3.Connection, signer: anchor.web3.PublicKey): Promise<any> => {
+  const getPdaParams = async (connection: anchor.web3.Connection, signer: anchor.web3.PublicKey, mintNFT: anchor.web3.PublicKey): Promise<any> => {
     const uid = new anchor.BN(parseInt((Date.now() / 1000).toString()));
     const uidBuffer = uid.toBuffer("le", 8);
 
-    let [statePubKey, stateBump] = await anchor.web3.PublicKey.findProgramAddress([Buffer.from("state"), signer.toBuffer(), uidBuffer], program.programId);
+    let [statePubKey, stateBump] = await anchor.web3.PublicKey.findProgramAddress([Buffer.from("state"), signer.toBuffer(), mintNFT.toBuffer(), uidBuffer], program.programId);
+    let [walletPubKey, walletBump] = await anchor.web3.PublicKey.findProgramAddress(
+      [Buffer.from("wallet"), signer.toBuffer(), mintNFT.toBuffer(), uidBuffer], program.programId,
+    );
     return {
       stateBump,
 
       idx: uid,
-      stateKey: statePubKey
+      stateKey: statePubKey,
+      escrowBump: walletBump,
+      escrowWalletKey: walletPubKey
     };
   };
 
   it("Initialize", async () => {
-    mintUSDT = await createNewMint(connection, payer, admin.publicKey, admin.publicKey, 6);
+    // Fund accounts with gas
+    await Promise.all([fundSolana(provider, admin.publicKey, 5), fundSolana(provider, buyer.publicKey, 5), fundSolana(provider, treasurer.publicKey, 5)]);
+
+    // create mint for USDT and VOT1 NFT
+    [mintUSDT, mintVOT1] = await Promise.all([createNewMint(connection, admin, admin.publicKey, admin.publicKey, 6), createNewMint(connection, admin, admin.publicKey, admin.publicKey, 0)]);
+
+    const [treasurerTokenAccount, buyerTokenAccount] = await Promise.all([
+      createTokenAccount(connection, treasurer, mintVOT1, treasurer.publicKey),
+      createTokenAccount(connection, buyer, mintUSDT, buyer.publicKey)
+    ]);
+
+    await Promise.all([
+      // Mint 200k VOT1 NFT to treasurer
+      mintTokens(connection, treasurer, mintVOT1, treasurerTokenAccount.address, admin, NFT_TOTAL_SUPPLY),
+      // Mint 1000 USDT to buyer
+      mintTokens(connection, buyer, mintUSDT, buyerTokenAccount.address, admin, 1000 * 1000000)
+    ]);
+
+    pda = await getPdaParams(connection, treasurer.publicKey, mintVOT1);
+
     expect(mintUSDT.toBase58());
   });
 
-  it("Setup", async () => {
-    const pda = await getPdaParams(connection, provider.wallet.publicKey);
+  it("Prepare NFT vault", async () => {
+    const [walletToTakeNft] = await Promise.all([
+      createTokenAccount(connection, treasurer, mintVOT1, treasurer.publicKey)
+    ]);
 
-    const tx = await program.methods.setTreasury(new anchor.BN(pda.idx)).accounts({
-      signer: provider.wallet.publicKey,
-      systemProgram: anchor.web3.SystemProgram.programId,
-      applicationState: pda.stateKey,
-      treasury: new anchor.web3.PublicKey(TREASURY_WALLET)
-    }).rpc();
-
-    const applicationState = await program.account.state.fetch(pda.stateKey);
-    console.log(applicationState.treasury.toBase58())
+    await program.methods
+      .depositNft(new anchor.BN(pda.idx), pda.stateBump, new anchor.BN(NFT_TOTAL_SUPPLY))
+      .accounts({
+        treasurer: walletToTakeNft.owner,
+        mintOfTokenBeingSent: mintVOT1,
+        mintOfTokenBeingPaid: mintUSDT,
+        walletToWithdrawFrom: walletToTakeNft.address,
+        escrowWalletState: pda.escrowWalletKey,
+        applicationState: pda.stateKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+        tokenProgram: token.TOKEN_PROGRAM_ID,
+        rent: anchor.web3.SYSVAR_RENT_PUBKEY
+      })
+      .signers([treasurer])
+      .rpc();
   });
 
-  it("Buying", async () => {
-    // const tx = await program.methods.setTreasury(101).accounts({ user: provider.wallet.publicKey }).rpc();
+  it("Buy NFT", async () => {
+    const [walletToWithdrawFrom, walletToDepositTo, walletToSendTo] = await Promise.all([
+      createTokenAccount(connection, buyer, mintUSDT, buyer.publicKey),
+      createTokenAccount(connection, treasurer, mintUSDT, treasurer.publicKey),
+      createTokenAccount(connection, buyer, mintVOT1, buyer.publicKey)
+    ]);
+
+    await program.methods
+      .buy(new anchor.BN(pda.idx), pda.stateBump, pda.escrowBump, new anchor.BN(10))
+      .accounts({
+        treasurer: treasurer.publicKey,
+        mintOfTokenBeingPaid: mintUSDT,
+        mintOfTokenBeingSent: mintVOT1,
+        buyer: walletToWithdrawFrom.owner,
+        walletToWithdrawFrom: walletToWithdrawFrom.address,
+        walletToDepositTo: walletToDepositTo.address,
+        walletToSendTo: walletToSendTo.address,
+        escrowWalletState: pda.escrowWalletKey,
+        applicationState: pda.stateKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+        tokenProgram: token.TOKEN_PROGRAM_ID,
+        associatedTokenProgram: token.ASSOCIATED_TOKEN_PROGRAM_ID,
+        rent: anchor.web3.SYSVAR_RENT_PUBKEY
+      })
+      .signers([buyer])
+      .rpc();
+
+    const balance = await connection.getTokenAccountBalance(walletToDepositTo.address);
+    console.log(balance.value.amount);
+
+    const balance2 = await connection.getTokenAccountBalance(walletToSendTo.address);
+    console.log(balance2);
   });
 });

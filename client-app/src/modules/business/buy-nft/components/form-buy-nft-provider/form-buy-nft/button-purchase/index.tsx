78a1a35be626e2f179c19c919b40d3cc267a7ab2
Quang Doan <quang.doan@metain.io>
2022-12-05 02:09:23 +0700

    Update index.tsx

--- client-app/src/modules/business/buy-nft/components/form-buy-nft-provider/form-buy-nft/button-purchase/index.tsx
+++ client-app/src/modules/business/buy-nft/components/form-buy-nft-provider/form-buy-nft/button-purchase/index.tsx
@@ -5,25 +5,29 @@ import styles from './styles.module.scss';
 const ButtonPurchase = () => {
     const { status, error, handlePurchaseNft } = useFormBuyNftContext();
 
+    const getButtonText = () => {
+        switch (status) {
+            case FormBuyNftStatus.Initializing:
+                return 'Initializing...';
+            case FormBuyNftStatus.InitializeSucceeded:
+                return 'Purchase';
+            case FormBuyNftStatus.Processing:
+                return 'Processing...';
+            case FormBuyNftStatus.ProcessSucceeded:
+                return 'Purchase Succeeded';
+            case FormBuyNftStatus.InitializeFailed:
+            case FormBuyNftStatus.ProcessFailed:
+                return error;
+        }
+    };
+
     return (
         <button
             className={['mButton mButton-cn1-bp6', styles.button].join(' ')}
             onClick={handlePurchaseNft}
             disabled={status == FormBuyNftStatus.Initializing || status == FormBuyNftStatus.Processing}
         >
-            {status == FormBuyNftStatus.Initializing
-                ? 'Initializing...'
-                : status == FormBuyNftStatus.InitializeFailed
-                ? error
-                : status == FormBuyNftStatus.InitializeSucceeded
-                ? 'Purchase'
-                : status == FormBuyNftStatus.Processing
-                ? 'Processing...'
-                : status == FormBuyNftStatus.ProcessFailed
-                ? error
-                : status == FormBuyNftStatus.ProcessSucceeded
-                ? 'Purchase Succeeded'
-                : 'Purchase'}
+            {getButtonText()}
         </button>
     );
 };

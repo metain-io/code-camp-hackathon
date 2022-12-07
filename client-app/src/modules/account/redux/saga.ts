5a73d715db53f5af02c05443cca0dbab4d6294c2
Quang Doan <quang.doan@metain.io>
2022-12-07 17:04:10 +0700

    Update saga.ts

--- client-app/src/modules/account/redux/saga.ts
+++ client-app/src/modules/account/redux/saga.ts
@@ -1,15 +1,9 @@
-import { resolveGenerator } from '@libs/utils';
-import { all, call, fork, put, take, takeLatest } from 'redux-saga/effects';
-import { accountActions, TokenBalance } from './slice';
-import UserPoolService, { PoolTypeEnum } from '@auth/services/user-pool';
-import AuthService from '@auth/services/auth';
-import { eventChannel } from 'redux-saga';
-import logger from '@libs/logger';
-import CryptoWallet, { CryptoWalletEvent } from '@crypto-wallet/crypto-wallet';
-import PhantomWallet from '@crypto-wallet/wallet-adapters/phantom-wallet-adapter';
-import WalletService from '@crypto-wallet/services/crypto-wallet-service';
-import MetainApi from '@api/metain';
+import { getExchangeRateList } from '@api/metain/entry-points/oracle';
 import { ExchangeRateItem } from '@business/dashboard/hooks/use-dashboard';
+import WalletService from '@crypto-wallet/services/crypto-wallet-service';
+import logger from '@libs/logger';
+import { all, put, takeLatest } from 'redux-saga/effects';
+import { accountActions, TokenBalance } from './slice';
 
 function* handleGetTokenBalanceRequested(): any {
     logger.info('saga/account/handleGetTokenBalanceRequested');
@@ -17,32 +11,32 @@ function* handleGetTokenBalanceRequested(): any {
     try {
         const [tmpBalances, exchangeRateList] = yield all([
             WalletService._currentWallet?.getBalances(),
-            MetainApi.oracle.getExchangeRateList(),
+            getExchangeRateList(),
         ]);
 
         const entries = Object.entries(tmpBalances);
         const tokenBalances: { [symbol: string]: TokenBalance } = {};
 
         entries &&
-        entries.length > 0 &&
-        entries.forEach((item, idx: number) => {
-            const exchangeRateItem: ExchangeRateItem | undefined = exchangeRateList?.find(
-                (value: ExchangeRateItem, index: number) => {
-                    return value?.coin === item[0];
-                },
-            );
-            const currentPrice = exchangeRateItem?.max_usdt_rate ?? 0;
-            const tokenAmount: number = item[1] as number
+            entries.length > 0 &&
+            entries.forEach((item, idx: number) => {
+                const exchangeRateItem: ExchangeRateItem | undefined = exchangeRateList?.find(
+                    (value: ExchangeRateItem, index: number) => {
+                        return value?.coin === item[0];
+                    },
+                );
+                const currentPrice = exchangeRateItem?.max_usdt_rate ?? 0;
+                const tokenAmount: number = item[1] as number;
 
-            tokenBalances[item[0]] = {
-                price: currentPrice,
-                amount: tokenAmount,
-            };
-        });
+                tokenBalances[item[0]] = {
+                    price: currentPrice,
+                    amount: tokenAmount,
+                };
+            });
 
         yield put(accountActions.getTokenBalanceSucceed(tokenBalances));
     } catch (error: any) {
-        logger.error('saga/account/handleGetTokenBalanceRequested/ERROR: ', error)
+        logger.error('saga/account/handleGetTokenBalanceRequested/ERROR: ', error);
     }
 }
 
@@ -50,26 +44,26 @@ function* handleGetNftBalanceRequested(): any {
     logger.info('saga/account/handleGetNftBalanceRequested');
 
     try {
-        const tmpNftBalance = yield (WalletService._currentWallet?.getNftBalance());
+        const tmpNftBalance = yield WalletService._currentWallet?.getNftBalance();
 
         const entries = Object.entries(tmpNftBalance);
         const nftBalances: { [symbol: string]: TokenBalance } = {};
 
         entries &&
-        entries.length > 0 &&
-        entries.forEach((item, idx: number) => {
-            const currentPrice = 10;        // TODO: Temporary hard code
-            const tokenAmount: number = item[1] as number
+            entries.length > 0 &&
+            entries.forEach((item, idx: number) => {
+                const currentPrice = 10; // TODO: Temporary hard code
+                const tokenAmount: number = item[1] as number;
 
-            nftBalances[item[0]] = {
-                price: currentPrice,
-                amount: tokenAmount,
-            };
-        });
-        
+                nftBalances[item[0]] = {
+                    price: currentPrice,
+                    amount: tokenAmount,
+                };
+            });
+
         yield put(accountActions.getNftBalanceSucceed(nftBalances));
     } catch (error: any) {
-        logger.error('saga/account/handleGetNftBalanceRequested/ERROR: ', error)
+        logger.error('saga/account/handleGetNftBalanceRequested/ERROR: ', error);
     }
 }
 

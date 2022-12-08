32c85ed590bb485808fff3fafc31d7658bbf639f
quan <quan.phanngocminh.111@gmail.com>
2022-12-08 11:03:14 +0700

    - update admin page

--- client-app/src/modules/business/share-dividend/components/form-share-devidend/index.tsx
+++ client-app/src/modules/business/share-dividend/components/form-share-devidend/index.tsx
@@ -1,9 +1,28 @@
-import { ChangeEvent, MouseEvent } from 'react';
+import axios from '@api/metain/axios-client';
+import { ChangeEvent, MouseEvent, useState, useEffect } from 'react';
 import { DividendHistoryToggler } from './dividend-history-toggler';
 import styles from './styles.module.scss';
 import { useShareDividend } from './use-share-dividend';
+import moment from 'moment';
 
 const FormShareDividend = () => {
+    const [currentDemoDate, setCurrentDemoDate] = useState('');
+    const [newDemoDate, setNewDemoDate] = useState('2023-01-01');
+    const [dividendFromDate, setDividendFromDate] = useState('2023-01-01');
+    const [dividendToDate, setDividendToDate] = useState('2023-01-01');
+    const [dividendAmount, setDividendAmount] = useState('0.1');
+
+    const init = async () => {
+        const result = await axios.get('https://api.niatem-beta.com/hackathon/get-offset-times');
+        const dt = moment((result as any).body.counter).format('YYYY-MM-DD');
+        setNewDemoDate(dt);
+        setCurrentDemoDate(dt);
+    };
+
+    useEffect(() => {
+        init();
+    }, []);
+
     const { handleAmountDevidendChanged, amountDevidend, handleShareDividend, handleNextDay, handleReset } =
         useShareDividend();
 
@@ -11,9 +30,14 @@ const FormShareDividend = () => {
         handleAmountDevidendChanged(e.target.value);
     };
 
-    const onButtonShareDividendClicked = (e: MouseEvent<HTMLButtonElement>) => {
+    const onButtonShareDividendClicked = async (e: MouseEvent<HTMLButtonElement>) => {
         e.preventDefault();
-        handleShareDividend();
+        const result = await axios.post('https://api.niatem-beta.com/hackathon/share-dividend', {
+            from: moment(dividendFromDate, 'YYYY-MM-DD').valueOf(),
+            to: moment(dividendToDate, 'YYYY-MM-DD').valueOf(),
+            value: dividendAmount,
+        });
+        console.log(result);
     };
 
     const onButtonNextDayClicked = (e: MouseEvent<HTMLButtonElement>) => {
@@ -26,31 +50,57 @@ const FormShareDividend = () => {
         handleReset();
     };
 
+    const setDemoDate = async (e: MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        const result = await axios.post('https://api.niatem-beta.com/hackathon/set-offset-timestamp', {
+            timestamp: moment(newDemoDate, 'YYYY-MM-DD').valueOf(),
+        });
+        console.log(result);
+        setCurrentDemoDate(newDemoDate);
+    };
+
     return (
-        <form className={styles['form']}>
-            <div className={styles['form-header']}>
-                <label className={styles['form-title']}>See Share Dividend History: </label>
-
+        <div>
+            <form className={styles['form']}>
                 <div>
-                    <DividendHistoryToggler />
+                    <label>Demo date:</label>
+                    <input readOnly value={currentDemoDate} />
+
+                    <input value={newDemoDate} type="date" onChange={(e) => setNewDemoDate(e.target.value)} />
+                    <button type="button" onClick={setDemoDate}>
+                        Set Demo Date
+                    </button>
                 </div>
-            </div>
 
-            <div className={styles['form-group']}>
-                <label>Devidend for 1 NFT / day</label>
-                <input value={amountDevidend} onChange={onInputAmountDividendChanged} />
-            </div>
+                <hr />
 
-            <button onClick={onButtonShareDividendClicked}>Share Dividend</button>
+                <div className={styles['form-group']}>
+                    <label>Dividend for 1 NFT / day</label>
+                    <div>
+                        <label>From</label>
+                        <input
+                            type="date"
+                            value={dividendFromDate}
+                            onChange={(e) => setDividendFromDate(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <label>To</label>
+                        <input type="date" value={dividendToDate} onChange={(e) => setDividendToDate(e.target.value)} />
+                    </div>
+                    <div>
+                        <label>Amount</label>
+                        <input
+                            type="number"
+                            value={dividendAmount}
+                            onChange={(e) => setDividendAmount(e.target.value)}
+                        />
+                    </div>
 
-            <br></br>
-
-            <button onClick={onButtonResetClicked}>Next Day</button>
-
-            <br></br>
-
-            <button onClick={onButtonNextDayClicked}>Reset Demo</button>
-        </form>
+                    <button onClick={onButtonShareDividendClicked}>Share Dividend</button>
+                </div>
+            </form>
+        </div>
     );
 };
 

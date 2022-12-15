3d828ad9425cc5c06a76d3b268a2d3e1d1352a70
quan <quan.phanngocminh.111@gmail.com>
2022-12-15 14:05:33 +0700

    - remove mint-nft function in menu

--- client-app/src/modules/app/layouts/admin-layout/navigation/index.tsx
+++ client-app/src/modules/app/layouts/admin-layout/navigation/index.tsx
@@ -48,12 +48,12 @@ const Navigation = () => {
                         url: 'wallet',
                         className: '',
                     },
-                    {
-                        name: 'Mint NFT',
-                        icon: 'fms fm-usd-circle',
-                        url: 'min-nft',
-                        className: '',
-                    },
+                    // {
+                    //     name: 'Mint NFT',
+                    //     icon: 'fms fm-usd-circle',
+                    //     url: 'min-nft',
+                    //     className: '',
+                    // },
                 ],
             },
             {
